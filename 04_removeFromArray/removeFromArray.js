7d48a80346498887ebb0fd1792343c1d94ae24a9
Hersh <sanganihersh@gmail.com>
2024-07-16 12:14:53 -0400

    Create initial for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, removeList) {
+    
+    // For each element in the removeList, remove all instances of that from the inputArray
+
 };
 
 // Do not edit below this line
