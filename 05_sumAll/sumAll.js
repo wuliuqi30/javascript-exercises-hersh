fbc9a743ce3328640ad93f29dcbf623fc3e1881a
Hersh <sanganihersh@gmail.com>
2024-07-17 19:15:50 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(n,m) {
+
+    if ((typeof n !== 'number') || (typeof m !== 'number')) {
+        console.log("Inputs must be numbers!");
+        return 'ERROR';
+    } else if (n <= 0 || m <= 0){
+        console.log("Inputs must be positive!")
+        return 'ERROR';
+    } else if (!Number.isInteger(n) || !Number.isInteger(m)){
+        console.log("Inputs must be integers!")
+        return 'ERROR';
+    } else {
+        let sum = 0;
+        let startInt = n;
+        let endInt = m;
+        // Make sure n is less than or equal to m:
+        if (n > m){
+            startInt = m;
+            endInt = n;
+        }
+        for(let i = startInt; i <= endInt; i++){
+            sum += i;
+        }
+        return sum;
+
+    }
+
 
 };
 
