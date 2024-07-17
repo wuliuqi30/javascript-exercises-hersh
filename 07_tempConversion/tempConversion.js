f8f0bfda207791356d447efd53795db610db7dec
Hersh <sanganihersh@gmail.com>
2024-07-17 19:53:10 -0400

    Complete Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Tf) {
+  let TcUnrounded = (Tf - 32) * (5/9) ;
+
+  return Number(TcUnrounded.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Tc) {
+  let TfUnrounded =  (Tc * 9/5) + 32;
+  return Number(TfUnrounded.toFixed(1));
 };
 
 // Do not edit below this line
