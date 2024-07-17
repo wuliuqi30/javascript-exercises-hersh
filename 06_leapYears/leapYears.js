48d08efe320a705b27780bc96e980fde84a0b627
Hersh <sanganihersh@gmail.com>
2024-07-17 19:31:52 -0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // A leap year is either divisible by 400 or divisible by 4 and not 100
+    if ( (year % 400 === 0) || 
+    ( (year % 4 === 0) && !(year % 100 === 0) )){
+        return true;
+    } else {
+        return false;
+    }
+
 
 };
 
