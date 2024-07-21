82c24e089d797d135db4bd084b688230bc316713
Hersh <sanganihersh@gmail.com>
2024-07-21 15:01:06 -0400

    Complete and Pass Tests for Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    console.log(str);
+    let letters = /^[A-Za-z0-9]+$/;
+
+    let justLettersArray = str
+        .toLowerCase()
+        .split('')
+        .filter(character =>
+            character.match(letters));
+
+    strLength = justLettersArray.length;
+    for (c = 0; c < Math.floor(strLength/ 2); c++) {
+        console.log(`c is ${c}`);
+        console.log(`justLettersArray[c] is ${justLettersArray[c]} and justLettersArray[strLength-1-c] is ${justLettersArray[strLength-1-c]}`);
+        if (justLettersArray[c] !== justLettersArray[strLength-1-c]){
+            console.log('found a mismatch');
+            return false;
+        }
+    }
+
+    // console.table(justLettersArray);
+    // let justLettersString = justLettersArray.join('');
+    // console.log(justLettersString);
+    return true;
 
 };
 
