a4fa589008fe552dfa695ac6f1c227021ccd37ee
Hersh <sanganihersh@gmail.com>
2024-07-21 15:24:35 -0400

    Finish Fibonacci and Complete Tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (nInput) {
+
+
+    // if a string, convert to a number:
+    const n = +nInput;
+
+    olderNumber = 1;
+    newerNumber = 1;
+    if (n < 0){
+        return "OOPS";
+    }
+    if (n == 0){
+        return 0;
+    }
+    if (n <= 2 ) {
+        return 1;
+    } 
+
+    let temp = 0;
+    for (i = 3; i <= n; i++ ){
+        console.log(`olderNumber ${olderNumber}, newerNumber ${newerNumber}`)
+        temp = olderNumber + newerNumber;
+        olderNumber = newerNumber;
+        newerNumber = temp;
+        console.log(`Converts to olderNumber ${olderNumber}, newerNumber ${newerNumber}`)
+    }
+    return newerNumber;
 
 };
 
